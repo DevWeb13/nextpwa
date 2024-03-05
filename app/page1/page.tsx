3d6525dbeb6f0924b363f44fbf0e6cda302d299f
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-05 15:06:45 +0100

    Add demo with pages

---
+++ app/page1/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function Page1() {
+  return (
+    <main
+      className='flex min-h-screen flex-col items-center justify-between p-12 
+     md:p-24 gap-8
+    '>
+      <h1 className='text-4xl font-bold text-center text-gray-900 mb-8'>
+        {' '}
+        Page1
+      </h1>
+      <Link
+        className='bg-blue-500 text-white px-4 py-2 rounded-md'
+        href='/'>
+        Retour
+      </Link>
+    </main>
+  );
+}
